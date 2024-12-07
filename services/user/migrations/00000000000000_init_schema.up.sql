19da16ceafe05bd8cf571edef232d2039b0a44e4
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2024-12-07 20:38:07 +0700

    Init userserver and database schema

---
+++ services/user/migrations/00000000000000_init_schema.up.sql
@@ -0,0 +1,43 @@
+
+-- USERS --
+
+CREATE TABLE public.users
+(
+    user_id         UUID                        NOT NULL,
+    first_name      VARCHAR(255)                NOT NULL,
+    last_name       VARCHAR(255)                NOT NULL,
+    status          INT                         NOT NULL,
+    created_at      TIMESTAMP WITH TIME ZONE    NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE    NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+-- USER CREDENTIALS --
+
+CREATE TABLE public.user_credentials
+(
+    user_id         UUID                        NOT NULL,
+    auth_provider   VARCHAR(255)                NOT NULL,
+    meta            JSONB,
+    created_at      TIMESTAMP WITH TIME ZONE    NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE    NOT NULL,
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_user_credentials_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+-- USER SESSIONS --
+
+CREATE TABLE public.user_sessions
+(
+    session_id          BIGSERIAL                   NOT NULL,
+    user_id             UUID                        NOT NULL,
+    token               VARCHAR(255)                NOT NULL,
+    session_start       TIMESTAMP WITH TIME ZONE    NOT NULL,
+    last_updated        TIMESTAMP WITH TIME ZONE    NOT NULL,
+    session_expiration  TIMESTAMP WITH TIME ZONE,
+    session_end         TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (session_id),
+    CONSTRAINT fk_user_sessions_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE INDEX ON user_sessions (token);
