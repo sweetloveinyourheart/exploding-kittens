3322450ad6a3e61ecdafcd3aa54df8909329e0e2
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2024-12-16 00:23:43 +0700

    Update generate script

--- scripts/gen/run_code_generators.sh
+++ scripts/gen/run_code_generators.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 . ./scripts/util.sh
 
+set -e
+
+function resetFiles() {
+    pocker-echo "Resetting files matching $1"
+    local filePattern=$1
+    local files=$(find proto -name "$filePattern" -type f)
+    for file in $files; do
+        git checkout --ours $file &> /dev/null || ( git checkout --theirs $file &> /dev/null || : ) # :)
+    done
+}
+
+resetFiles "*.pb.go"
+resetFiles "*.connect.go"
+
 goGenerateCmd="go generate --tags generate ./..."
 goImportsCmd="go run golang.org/x/tools/cmd/goimports --local "github.com/sweetloveinyourheart/planning-poker" -w ./"
 
