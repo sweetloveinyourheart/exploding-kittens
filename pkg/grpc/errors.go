ac9b3f0a9078c61af2f4042e87608b03b23c20f5
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2024-12-21 22:01:43 +0700

    Create GRPC errors pkg

---
+++ pkg/grpc/errors.go
@@ -0,0 +1,39 @@
+package grpc
+
+import (
+	"connectrpc.com/connect"
+	"github.com/cockroachdb/errors"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+)
+
+func FieldViolation(field string, err error) *errdetails.BadRequest_FieldViolation {
+	return &errdetails.BadRequest_FieldViolation{
+		Field:       field,
+		Description: err.Error(),
+	}
+}
+
+func InvalidArgumentError(violations ...*errdetails.BadRequest_FieldViolation) error {
+	statusInvalid := connect.NewError(connect.CodeInvalidArgument, errors.New("invalid parameters"))
+	for _, violation := range violations {
+		if detail, detailErr := connect.NewErrorDetail(violation); detailErr == nil {
+			statusInvalid.AddDetail(detail)
+		}
+	}
+
+	return statusInvalid
+}
+
+func InternalError(err error) error {
+	if connectErr := new(connect.Error); errors.As(err, &connectErr) {
+		return err
+	}
+	return connect.NewError(connect.CodeInternal, err)
+}
+
+func NotFoundError(err error) error {
+	if connectErr := new(connect.Error); errors.As(err, &connectErr) {
+		return err
+	}
+	return connect.NewError(connect.CodeNotFound, errors.Newf("not found: %s", err))
+}
