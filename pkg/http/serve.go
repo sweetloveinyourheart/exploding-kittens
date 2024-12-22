8343949a51ecf98f69fea2ec901d1fad4ab24ec8
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2024-12-22 23:34:28 +0700

    Add http pkg

---
+++ pkg/http/serve.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	log "github.com/sweetloveinyourheart/planning-pocker/pkg/logger"
+)
+
+func ServeHTTP(ctx context.Context, handler *mux.Router, port uint64, serviceName string) {
+	log.GlobalSugared().Infof("HTTP %s listening on port %d\n", serviceName, port)
+
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", port),
+		Handler:           handler,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.GlobalSugared().Panicf("%s failed to serve: %v", serviceName, err)
+			} else {
+				log.GlobalSugared().Infof("HTTP %s server closed", serviceName)
+			}
+		}
+	}()
+	<-ctx.Done()
+	log.GlobalSugared().Infof("HTTP %s shutting down", serviceName)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.GlobalSugared().Panicf("%s failed to shutdown: %v", serviceName, err)
+	}
+}
