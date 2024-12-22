7b8b713594c5f4f73d9552df1b935ee4b216b3d5
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2024-12-23 00:05:36 +0700

    Implement GetUserByID

--- services/gateway/handlers/user_handler.go
+++ services/gateway/handlers/user_handler.go
@@ -3,18 +3,31 @@ package handlers
 import (
 	"net/http"
 
+	"connectrpc.com/connect"
 	"github.com/gorilla/mux"
 
+	grpc "github.com/sweetloveinyourheart/planning-pocker/proto/code/userserver/go"
 	"github.com/sweetloveinyourheart/planning-pocker/services/gateway/common/responses"
 )
 
 func (h *handler) GetUserByID(w http.ResponseWriter, r *http.Request) {
 	routerVars := mux.Vars(r)
+
 	userID := routerVars["id"]
 	if userID == "" {
 		responses.BadRequestException(w, "User ID is required")
 		return
 	}
 
-	responses.JSON(w, 200, "Get user successfully", true)
+	user, err := h.userServerClient.GetUser(h.ctx,
+		connect.NewRequest(&grpc.GetUserRequest{
+			UserId: userID,
+		}),
+	)
+	if err != nil {
+		responses.NotFoundException(w, err.Error())
+		return
+	}
+
+	responses.JSON(w, 200, "Get user successfully", user)
 }
