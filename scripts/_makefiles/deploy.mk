398f82285f75744de70e0f1182eef2131eaf26fa
Nguyen Lam <sweetloveinyourheart@gmail.com>
2025-08-12 12:07:54 +0700

    Add image-tag command

--- scripts/_makefiles/deploy.mk
+++ scripts/_makefiles/deploy.mk
@@ -1,9 +1,14 @@
+GHCR_URL=ghcr.io
+EXPLODING_KITTENS_REPO=sweetloveinyourheart
+
 push-to-ghcr:
 	@docker tag $(IMAGE_NAME) $(REPOSITORY_URI):$(IMAGE_TAG)
 	@docker push $(REPOSITORY_URI):$(IMAGE_TAG)
 
-GHCR_URL=ghcr.io
-EXPLODING_KITTENS_REPO=sweetloveinyourheart
+
+# Generate a new image tag
+image-tag:
+	@echo $(IMAGE_TAG)
 
 kittens-docker-push:
 	@make push-to-ghcr IMAGE_TAG=$(IMAGE_TAG) IMAGE_NAME=kittens:latest REPOSITORY_URI=$(GHCR_URL)/$(EXPLODING_KITTENS_REPO)
