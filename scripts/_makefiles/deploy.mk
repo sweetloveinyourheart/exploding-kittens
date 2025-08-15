fbbca7c8193017e52a4d10b630ace4e1cf55e757
Nguyen Lam <sweetloveinyourheart@gmail.com>
2025-08-15 15:38:47 +0700

    Add image tag generation script

--- scripts/_makefiles/deploy.mk
+++ scripts/_makefiles/deploy.mk
@@ -1,6 +1,16 @@
 GHCR_URL=ghcr.io
 EXPLODING_KITTENS_REPO=sweetloveinyourheart
 
+BRANCHTAG=$(shell git rev-parse --abbrev-ref HEAD)
+BRANCHTAG_SAFE=$(shell echo $(BRANCHTAG) | tr '[:upper:]' '[:lower:]' | tr '/' '_' | cut -c 1-32)
+GIT_COMMIT_HASH=$(shell git rev-parse --short HEAD | tr '[:upper:]' '[:lower:]')
+ifeq ("$(BRANCHTAG)", "HEAD")
+	BRANCHTAG=$(shell git branch | grep HEAD | cut -d' ' -f 5 | tr -d '()' | tr '/' '_' | xargs)
+	IMAGE_TAG=$(BRANCHTAG)
+else
+	IMAGE_TAG=$(shell date '+%Y-%m-%d-%H%M')_$(BRANCHTAG_SAFE)_$(GIT_COMMIT_HASH)
+endif
+
 push-to-ghcr:
 	@docker tag $(IMAGE_NAME) $(REPOSITORY_URI):$(IMAGE_TAG)
 	@docker push $(REPOSITORY_URI):$(IMAGE_TAG)
